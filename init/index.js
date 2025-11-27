731ce86438296b74e5362915e8531d553b8310d6
kanak-thapa <kanakthapa2027@gmail.com>
2025-11-27 23:58:47 +0530

    added essential updates

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require('../models/listings.js');
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLand";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
 .then(()=>{
