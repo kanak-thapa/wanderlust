42ab39d84579c295eaed058b22a6bda7d929874a
kanak-thapa <kanakthapa2027@gmail.com>
2025-11-27 21:12:19 +0530

    made changes in mongoid

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require('../models/listings.js');
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLand";
 
 main()
 .then(()=>{
@@ -16,10 +16,10 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
-const initDB = async()=>{
-    await Listing.deleteMany({});
-    initData.data=initData.data.map((obj)=>({...obj, owner: "671f122d055ec32fbf15a53c"}))
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
-}
-initDB();
+// const initDB = async()=>{
+//     await Listing.deleteMany({});
+//     initData.data=initData.data.map((obj)=>({...obj, owner: "671f122d055ec32fbf15a53c"}))
+//     await Listing.insertMany(initData.data);
+//     console.log("data was initialized");
+// }
+// initDB();
